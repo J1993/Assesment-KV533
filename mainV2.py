086bde38f2c85fbfa997f6e85a409a97ee59b8f7
Jordy Lopez <jordizeus93@gmail.com>
2025-11-10 12:28:45 +0000

    Initial

---
+++ mainV2.py
@@ -0,0 +1,50 @@
+
+from DO_NOT_LOOK_IN_HERE_FUTURE import *
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+
+    doMOREWORK(1)
+
+    test_question1_A()
+    testQuestion1_DO_NOT_ALTER_THIS_CODE()
+    test_question1_C()
+
+    doMOREWORK(1 )
+
+    test_question2_A()
+    testQuestion2_DO_NOT_ALTER_THIS_CODE()
+    test_question2_C()
+
+    doMOREWORK(1)
+
+    test_question3_A()
+    testQuestion3()
+    test_question3_C()
+
+    doMOREWORK(1)
+
+    test_question4_A()
+    testQuestion4()
+    test_question4_C()
+
+    doMOREWORK(1)
+
+    test_question5_A()
+    testQuestion5()
+    test_question5_C()
+
+    test_question6()
+    test_question7()
+    test_question8()
+
+    test_question9()
+
+
+    mk = getMark()
+    mx = getMaxMark()
+
+
+    print(f"Final Mark {mk} out of {mx}.#1")
+
